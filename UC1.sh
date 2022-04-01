979127cadb728a3c4fb34a7ac346044e55da28cf
Reshma2310 <dreshma253@gmail.com>
2022-04-01 22:03:26 +0500

    [Reshma]- Completed UC3

--- UC1.sh
+++ UC1.sh
@@ -7,5 +7,9 @@ read b
 echo "Enter Third input"
 read c
 
-echo "Output is $(( a + b * c ))"
+output1=$(( a + b * c ))
+echo "Output of UC2 is $output1"
+
+output2=$(( a * b + c ))
+echo "Output of UC3 is $output2"
 
