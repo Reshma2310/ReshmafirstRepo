7ee59983f67701f7d8b0eaa84848c5f930ddce95
Reshma2310 <dreshma253@gmail.com>
2022-04-02 11:08:04 +0500

    [Reshma]-Completed flipCoin UC2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
+#!/bin/bash
 
+i=1
+heads=0
+tails=0
+while [ $tails -lt 11 -a $heads -lt 11 ]
+do
 var=$(( RANDOM%2 ))
-if [ $var -eq 1 ]
-then
+	if [ $var -eq 1 ]
+	then
 	echo "Heads"
-else
+	heads=$(($heads + 1))
+	else
 	echo "Tails"
-fi
+	tails=$(($tails + 1))
+	fi
+((i++))
+done
+echo "No of Heads as output are $heads"
+echo "No of Tails as output are $tails"
