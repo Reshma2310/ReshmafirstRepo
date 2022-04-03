0a51cedc39f3cf5dc48bd061f2425553f1703176
Reshma2310 <dreshma253@gmail.com>
2022-04-03 19:55:15 +0500

    [Reshma]-Completed Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -17,3 +17,12 @@ do
 	res1[$i]=$(flip $i)
 done
 echo "Output of Singlet Combination is ${res1[@]}"
+#Doublet Combination
+for (( i=0; i<5; i++ ))
+do
+	for (( j=0; j<5; j++ ))
+	do
+	res2[$i]=$(flip $i)$(flip $j)
+	done
+done
+echo "Output of Doublet Combination is ${res2[@]}"
