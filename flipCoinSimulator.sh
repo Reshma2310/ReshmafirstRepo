30655a29835736bb1a301889b12f5435181c9934
Reshma2310 <dreshma253@gmail.com>
2022-04-03 11:00:04 +0500

    [Reshma]-Completed UC4

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 i=1
 heads=0
@@ -24,23 +24,19 @@ done
 echo "No of Heads as output are $heads"
 echo "No of Tails as output are $tails"
 
+#if heads=tails=21
 if [[ $heads -eq $tails ]]
 then
-	echo "equal"
-	flip
-	if [[ $heads -gt $tails+2 ]]
-	then
-		echo "Heads won by with $heads"
-	elif [[ $tails -gt $heads+2 ]]
-	then
-		echo "Tails won by with $tails"
-	else
-		echo "$heads $tails"
-	fi
-	((i++))
-elif [[ $heads -gt $tails ]]
+	while [[ $(($heads-$tails)) -lt 2 && $(($tails-$heads)) -lt 2 ]]
+	do
+		flip
+	done
+fi
+((i++))
+
+if [[ $heads -gt $tails ]]
 then
-	echo "Heads won by $(( $heads - $tails ))"
+	echo "Heads won by $heads"
 else
-	echo "Tails won by $(( $tails - $heads ))"
+	echo "Tails won by $tails"
 fi
